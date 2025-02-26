a44b453a29ffff549c52f5ac5740ac2d2600631c
Julio David Benitez Rios <113691935+Mbarakaj4@users.noreply.github.com>
2025-02-26 20:20:00 -0300

    Update dependencies installer

--- V3/modules/termux_dependencies.sh
+++ V3/modules/termux_dependencies.sh
@@ -12,4 +12,24 @@ echo -e "$ROJO ================================================ $LIMPIAR"
 echo -e "$CELESTE $BANNER \n"
 echo 'Dependency installer v1.0.0'
 echo -e "$ROJO ================================================ $LIMPIAR"
+
+# Preparacion
+DIRECTORIOJXLL="$HOME/teamjxll"
+echo 'Actualizando el sistema'
+pkg update & pkg upgrade -y &> /dev/null
+termux-setup-storage
+
+# Instalacion
+echo 'Instalando lenguajes de programacion...'
+pkg install python ruby php -y &> /dev/null
+echo 'Instalando librerias...'
+pkg install curl wget openssh openssl grep -y &> /dev/null
+echo 'Instalando apps...'
+pkg install tor w3m -y &> /dev/null
+echo 'Instalando Team_JXLL...'
+python -m venv $DIRECTORIOJXLL &> /dev/null
+
+echo 'Presione ENTER para continuar'
 read -n 1 -s
+source './JXLL_V3.sh'
+
