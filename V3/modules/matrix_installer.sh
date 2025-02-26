7f37626f1750845ec5391f351ef5c066b19ba723
Julio David Benitez Rios <113691935+Mbarakaj4@users.noreply.github.com>
2025-02-26 20:19:36 -0300

    Update matrix installer

--- V3/modules/matrix_installer.sh
+++ V3/modules/matrix_installer.sh
@@ -12,4 +12,15 @@ echo -e "$ROJO ================================================ $LIMPIAR"
 echo -e "$CELESTE $BANNER \n"
 echo 'CMatrix installer v1.0.0'
 echo -e "$ROJO ================================================ $LIMPIAR"
+
+# Preparacion
+pkg update
+pkg upgrade -y
+
+# Instalacion
+pkg install cmatrix
+
+# Post-Instalacion
+echo 'Presione ENTER para continuar'
 read -n 1 -s
+source './JXLL_V3.sh'
