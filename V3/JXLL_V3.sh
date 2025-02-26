f080325be0794ad09b1c70eeffd81ad2e582151d
Julio David Benitez Rios <113691935+Mbarakaj4@users.noreply.github.com>
2025-02-26 16:58:59 -0300

    Add modules integration and uninstall option

--- V3/JXLL_V3.sh
+++ V3/JXLL_V3.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Colores
 LIMPIAR='\e[0m'
 ROJO='\e[31m'
@@ -5,16 +7,45 @@ CELESTE='\e[36m'
 VERDE='\e[32m'
 
 # Textos
-opciones=('salir' 'instalar paquetes de termux' 'instalar banners' 'instalar efecto matrix')
+opciones=('Salir' 'Instalar paquetes de termux' 'Instalar banners'
+ 'Instalar efecto matrix' 'Desinstalar todo')
 
 # Banner
 BANNER=$(cat ./banners/banner_jxll.txt)
 echo -e "$ROJO ================================================ $LIMPIAR"
 echo -e "$CELESTE $BANNER \n"
 echo -e "$ROJO ================================================ $LIMPIAR"
-for index in 1 2 3 0
+
+# Opciones
+for index in 1 2 3 4 0
 do
     lista_ordenada=$(echo -e "$CELESTE [ $LIMPIAR $ROJO $index $LIMPIAR $CELESTE ] $LIMPIAR" |
      tr -d '[:blank:]')
     echo -e $lista_ordenada "$VERDE ${opciones[$index]} $LIMPIAR"
 done
+
+# Manejo de la respuesta del usuario
+echo -n ">>> "
+read -r opcion
+case $opcion in
+    1)
+     source "./modules/termux_dependencies.sh"
+     clear
+    ;;
+    2)
+     source "./modules/banner_installer.sh"
+     clear
+    ;;
+    3)
+     source "./modules/matrix_installer.sh"
+     clear
+    ;;
+    4)
+     source "./modules/uninstaller.sh"
+     clear
+    ;;
+    0)
+     clear
+     exit
+    ;;
+esac
