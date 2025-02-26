58ee502434af6b9e6045f09e2896974ed0a42e7b
Julio David Benitez Rios <113691935+Mbarakaj4@users.noreply.github.com>
2025-02-26 16:58:20 -0300

    Add modules to v3

---
+++ V3/modules/termux_dependencies.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Colores
+LIMPIAR='\e[0m'
+ROJO='\e[31m'
+CELESTE='\e[36m'
+VERDE='\e[32m'
+
+# Banner
+BANNER=$(cat ./banners/banner_jxll.txt)
+echo -e "$ROJO ================================================ $LIMPIAR"
+echo -e "$CELESTE $BANNER \n"
+echo 'Dependency installer v1.0.0'
+echo -e "$ROJO ================================================ $LIMPIAR"
+read -n 1 -s
