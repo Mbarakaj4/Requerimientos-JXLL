6c03eca579442e33b6983f6acc72a62f6f4da28b
Julio David Benitez Rios <113691935+Mbarakaj4@users.noreply.github.com>
2025-02-26 20:20:10 -0300

    Update uninstaller

--- V3/modules/uninstaller.sh
+++ V3/modules/uninstaller.sh
@@ -12,4 +12,21 @@ echo -e "$ROJO ================================================ $LIMPIAR"
 echo -e "$CELESTE $BANNER \n"
 echo 'Remove team jxll from your system'
 echo -e "$ROJO ================================================ $LIMPIAR"
+
+# Preparacion
+DIRECTORIOJXLL="$HOME/teamjxll"
+
+# Desinstalacion
+rm -r $DIRECTORIOJXLL
+pkg uninstall python ruby php \
+    curl wget openssh openssl grep \
+    tor w3m cmatrix -y
+
+# Post-Desinstalacion
+echo -e "$ROJO
+Lastimosamente el banner solo se puede remover manualmente.
+edite o restaure su archivo .bashrc ubicado en ~/.bashrc
+$LIMPIAR"
+echo 'Presione ENTER para continuar'
 read -n 1 -s
+source './JXLL_V3.sh'
